56a4685736fcb844aff4d5891fbc5c68f464e84f
hossananh-22 <hossanahnelsontestimony@gmail.com>
2025-08-31 22:18:05 +0100

    ready for production

--- agrocrest_farm/settings.py
+++ agrocrest_farm/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-$!es&^z41pm*^&#8s0jr!fn$2_k5!47_jz9$5hxz$ad33zgade
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["agrocrest.pythonanywhere.com"]
 
 AUTH_USER_MODEL = 'records.CustomUser'
 
